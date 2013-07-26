dd492f83cb123d576c76548abe552621719e994c
hkernbach <h.kernbach@triagens.de>
2013-07-26 20:37:05 +0200

    ...

--- assets/js/router/router.js
+++ assets/js/router/router.js
@@ -1,36 +1,31 @@
-/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true, newcap: true */
-/*global window, $, Backbone, document, arangoCollection, arangoHelper, dashboardView */
+/*jslint indent: 2, nomen: true, maxlen: 100, sloppy: true, vars: true, white: true, plusplus: true*/
+/*global window, $, Backbone */
 
-$(document).ready(function() {
+app.Router = Backbone.Router.extend({
 
-  window.Router = Backbone.Router.extend({
+  routes: {
+    ""       : "main",
+    "talks"   : "talks",
+    "speaker" : "speaker"
+  },
 
-    routes: {
-      ""       : "main",
-      "talks"   : "talks",
-      "speaker" : "speaker"
-    },
+  initialize: function () {
+    this.naviView = new app.navigationView();
+  },
 
-    initialize: {
-      this.naviView = new window.navigationView();
-    },
+  main: function () {
 
-    main: {
+  },
 
-    },
+  talks: function () {
 
-    talks: {
+  },
 
-    },
+  speaker: function () {
 
-    speaker: {
-
-    }
-
-  });
-
-  window.App = new window.Router();
-  Backbone.history.start();
+  }
 
 });
 
+app.router = new app.Router();
+Backbone.history.start();
